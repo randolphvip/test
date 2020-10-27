c954564bc729beff7e00a6ab7f8af6468111989d
randolphvip <shangcongvip@163.com>
2020-10-27 17:03:26 +1300

    d

---
+++ src/main/java/com/sc/test/FirstServelt.java
@@ -0,0 +1,29 @@
+package com.sc.test;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FirstServelt extends HttpServlet {
+	
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+		int a;
+		a=0;
+		int b=a/0;
+		String ab;
+		
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
